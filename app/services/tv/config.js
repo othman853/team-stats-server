a075bad27da10a35a6caf62c948c632b55e38b95
othman853 <anuar.yasser@gmail.com>
2017-04-28 14:20:10 -0300

    Promise forwarding on Config service

--- app/services/tv/config.js
+++ app/services/tv/config.js
@@ -4,15 +4,7 @@ module.exports = (ioService, timeService) => configDir => ({
       `${configDir}/config-${timeService.dateTime.now.asString()}.json`,
       ioService.json.stringify(config)
     )
-    .then(() => {
-      console.log(`Saved at: ${configDir}`);
-      console.log('Service.save => OK');
-      return resolve();
-    })
-    .catch(error => {
-      console.log(`Failed while trying to save at: ${configDir}`);
-      console.log('Service.save => FAIL');
-      return reject(error)
-    })
+    .then(resolve)
+    .catch(reject)
   )
 });
--- app/services/tv/index.js
+++ app/services/tv/index.js
@@ -2,4 +2,6 @@ const config = require('./config');
 const io = require('../io');
 const time = require('../time');
 
-module.exports = { createConfigService: config(io, time) };
+module.exports = {
+  createConfigService: config(io, time)
+};
